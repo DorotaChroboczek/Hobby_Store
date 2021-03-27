f709d16e3aaaf9bc5a7fd920f243073334fb9fd1
DorotaChroboczek <68025504+DorotaChroboczek@users.noreply.github.com>
2021-03-27 16:35:39 +0100

    feature: implement model Profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import CASCADE, DateTimeField, Model, OneToOneField
 
-# Create your models here.
+
+class Profile(Model):
+    user = OneToOneField(User, on_delete=CASCADE)
+    created = DateTimeField(auto_now_add=True)
