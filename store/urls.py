7b946f11e491ec0253721ae03464896f19a921fb
DorotaChroboczek <68025504+DorotaChroboczek@users.noreply.github.com>
2021-04-16 01:30:35 +0200

    in_progress: need to change on full path with path for users & name of category

--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,7 @@ app_name = 'store'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('product/<int:id>', StoreMetaProductView.as_view(), name='store_meta_product'),
     path('category/<int:id>/', CategoryView.as_view(), name='category'),
     path('category/<int:c_id>/subcategory/<int:sc_id>', SubcategoryView.as_view(), name='subcategory'),
     path('category/<int:c_id>/subcategory/<int:sc_id>/meta_product/<int:mp_id>', MetaProductView.as_view(), name='meta_product'),
--- store/views.py
+++ store/views.py
@@ -106,3 +106,27 @@ class MetaProductView(ListView):
             'c_sales': Category.objects.filter(overcategory__name='Sales').order_by('name').all()
         }
         return context
+
+
+class StoreMetaProductView(ListView):
+    template_name = 'store_meta_product.html'
+    model = MetaProduct
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        meta_product = get_object_or_404(MetaProduct, id=self.kwargs['id'])
+        products = Product.objects.filter(meta_product=meta_product).all()
+        context = {
+            'meta_product': meta_product,
+            'products': products,
+            'overcategories': Overcategory.objects.all(),
+            'c_modelings': Category.objects.filter(overcategory__name='Modeling').order_by('name').all(),
+            'c_sculptures': Category.objects.filter(overcategory__name='Sculpture').order_by('name').all(),
+            'c_paintings': Category.objects.filter(overcategory__name='Painting').order_by('name').all(),
+            'c_photos': Category.objects.filter(overcategory__name='Photography').order_by('name').all(),
+            'c_writings': Category.objects.filter(overcategory__name='Writing').order_by('name').all(),
+            'c_landscapes': Category.objects.filter(overcategory__name='Landscape Modeling').order_by('name').all(),
+            'c_giftsets': Category.objects.filter(overcategory__name='Gift Sets').order_by('name').all(),
+            'c_sales': Category.objects.filter(overcategory__name='Sales').order_by('name').all()
+        }
+        return context
