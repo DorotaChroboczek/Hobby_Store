5129ec8daa122849da8a44a77e73967596764b64
DorotaChroboczek <68025504+DorotaChroboczek@users.noreply.github.com>
2021-04-15 18:43:35 +0200

    feature: implement loop for categories filtered by overcategory

--- store/views.py
+++ store/views.py
@@ -12,12 +12,21 @@ class HomeView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
         context.update({
             'overcategories': Overcategory.objects.all(),
-            'categories': Category.objects.all()
+            'c_modelings': Category.objects.filter(overcategory__name='Modeling').order_by('name').all(),
+            'c_sculptures': Category.objects.filter(overcategory__name='Sculpture').order_by('name').all(),
+            'c_paintings': Category.objects.filter(overcategory__name='Painting').order_by('name').all(),
+            'c_photos': Category.objects.filter(overcategory__name='Photography').order_by('name').all(),
+            'c_writings': Category.objects.filter(overcategory__name='Writing').order_by('name').all(),
+            'c_landscapes': Category.objects.filter(overcategory__name='Landscape Modeling').order_by('name').all(),
+            'c_giftsets': Category.objects.filter(overcategory__name='Gift Sets').order_by('name').all(),
+            'c_sales': Category.objects.filter(overcategory__name='Sales').order_by('name').all(),
         })
         return context
 
     def get_queryset(self):
         return MetaProduct.objects.all().order_by('name')
+
 
