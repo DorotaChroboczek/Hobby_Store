b19ad443ae3bd863f9d0d62dd0919a3ae4499e2c
DorotaChroboczek <68025504+DorotaChroboczek@users.noreply.github.com>
2021-04-20 20:57:05 +0200

    feature: update SubategoryView context - cart

--- store/views.py
+++ store/views.py
@@ -89,7 +89,7 @@ class CategoryView(ContextListView):
         return context
 
 
-class SubcategoryView(ListView):
+class SubcategoryView(ContextListView):
     template_name = 'subcategory.html'
     model = Subcategory
 
@@ -98,20 +98,11 @@ class SubcategoryView(ListView):
         category = get_object_or_404(Category, id=self.kwargs['c_id'])
         subcategory = get_object_or_404(Subcategory, id=self.kwargs['sc_id'])
         meta_products = MetaProduct.objects.filter(subcategory=self.kwargs['sc_id']).order_by('name').all()
-        context = {
+        context.update({
             'category': category,
             'subcategory': subcategory,
             'meta_products': meta_products,
-            'overcategories': Overcategory.objects.all(),
-            'c_modelings': Category.objects.filter(overcategory__name='Modeling').order_by('name').all(),
-            'c_sculptures': Category.objects.filter(overcategory__name='Sculpture').order_by('name').all(),
-            'c_paintings': Category.objects.filter(overcategory__name='Painting').order_by('name').all(),
-            'c_photos': Category.objects.filter(overcategory__name='Photography').order_by('name').all(),
-            'c_writings': Category.objects.filter(overcategory__name='Writing').order_by('name').all(),
-            'c_landscapes': Category.objects.filter(overcategory__name='Landscape Modeling').order_by('name').all(),
-            'c_giftsets': Category.objects.filter(overcategory__name='Gift Sets').order_by('name').all(),
-            'c_sales': Category.objects.filter(overcategory__name='Sales').order_by('name').all()
-        }
+        })
         return context
 
 
