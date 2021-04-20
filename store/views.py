ab95425c8284da9c3f6c5f85512d7ea71d545614
DorotaChroboczek <68025504+DorotaChroboczek@users.noreply.github.com>
2021-04-20 20:56:08 +0200

    feature: update CategoryView context - cart, edit old version

--- store/views.py
+++ store/views.py
@@ -82,19 +82,10 @@ class CategoryView(ContextListView):
         context = super().get_context_data(**kwargs)
         category = get_object_or_404(Category, id=self.kwargs['id'])
         subcategories = Subcategory.objects.filter(category=self.kwargs['id']).order_by('name').all()
-        context = {
+        context.update({
             'category': category,
             'subcategories': subcategories,
-            'overcategories': Overcategory.objects.all(),
-            'c_modelings': Category.objects.filter(overcategory__name='Modeling').order_by('name').all(),
-            'c_sculptures': Category.objects.filter(overcategory__name='Sculpture').order_by('name').all(),
-            'c_paintings': Category.objects.filter(overcategory__name='Painting').order_by('name').all(),
-            'c_photos': Category.objects.filter(overcategory__name='Photography').order_by('name').all(),
-            'c_writings': Category.objects.filter(overcategory__name='Writing').order_by('name').all(),
-            'c_landscapes': Category.objects.filter(overcategory__name='Landscape Modeling').order_by('name').all(),
-            'c_giftsets': Category.objects.filter(overcategory__name='Gift Sets').order_by('name').all(),
-            'c_sales': Category.objects.filter(overcategory__name='Sales').order_by('name').all()
-        }
+        })
         return context
 
 
